5b59bc20cd359ce8875c29505e73a4230cb37aaa
yogo254 <yogo254@yahoo.com>
2023-08-12 00:17:15 +0300

    fix infinit add

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
+import sys
 
+def add_arguments(arguments):
+    total = 0
+    for arg in arguments:
+        total += int(arg)
+    return total
 
-if __name__ == '__main__':
-    from sys import argv
+if __name__ == "__main__":
+    arguments = sys.argv[1:]
+    result = add_arguments(arguments)
+    print(result)
 
-    sum = 0
-    for i, arg in enumerate(argv[1:]):
-        sum += int(arg)
-
-    print('{:d}'.format(sum))
