08f40397b1709dc7bad3e36f248c5ebf9bdd5a45
yogo254 <yogo254@yahoo.com>
2023-08-12 00:09:21 +0300

    fix progrm args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,9 +5,9 @@ if __name__ == '__main__':
     import sys
     num_args = len(sys.argv) - 1
 
-    if num_args == 1:
+    if num_args == 0:
         print('0 arguments.')
-    elif num_args == 2:
+    elif num_args == 1:
         print('{:d} argument:'.format(num_args))
     else:
         print('{:d} arguments:'.format(num_args))
