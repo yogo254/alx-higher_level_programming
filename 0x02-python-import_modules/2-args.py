02c6c2e48e179223bf92e69e532a244b5bdc47f4
yogo254 <yogo254@yahoo.com>
2023-08-12 00:04:53 +0300

    fix progrm args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
 
+
 if __name__ == '__main__':
-    from sys import argv
+    import sys
+    num_args = len(sys.argv) - 1
 
-    if len(argv) == 1:
+    if num_args == 1:
         print('0 arguments.')
-    elif len(argv) == 2:
-        print('{:d} argument:'.format(len(argv) - 1))
+    elif num_args == 2:
+        print('{:d} argument:'.format(num_args))
     else:
-        print('{:d} arguments:'.format(len(argv) - 1))
+        print('{:d} arguments:'.format(num_args))
 
-    for i, arg in enumerate(argv[1:], start=1):
-        print('{:d}: {:s}'.format(i, arg))
+    for i in range(1, num_args+1):
+        print('{:d}: {:s}'.format(i, sys.argv[i]))
