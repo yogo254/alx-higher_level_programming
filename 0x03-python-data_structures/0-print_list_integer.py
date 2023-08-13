2e57b94176647679043e97bcd50a904d300801be
yogo254 <yogo254@yahoo.com>
2023-08-13 20:58:37 +0300

    add readme and print number list

---
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+if __name__ == '__main__':
+    def print_list_integer(my_list=[]):
+        for n in my_list:
+            print('{:d}'.format(n))
---
+++ 0x03-python-data_structures/README.md
@@ -0,0 +1,2 @@
+# Python Data Structures
+- Python collections
