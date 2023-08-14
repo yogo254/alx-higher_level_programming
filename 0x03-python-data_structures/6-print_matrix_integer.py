7736bfa17810f05f159144b26ef2d9fb81440de2
yogo254 <yogo254@yahoo.com>
2023-08-14 21:41:31 +0300

    add print matrix

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+if __name__ == '__main__':
+    def print_matrix_integer(matrix=[[]]):
+        if len(matrix[0]) != 0:
+            rows = len(matrix)
+            columns = len(matrix[0])
+            for r in range(rows):
+                for c in range(columns):
+                    if c == columns - 1:
+                        print('{:d}'.format(matrix[r][c]))
+                    else:
+                        print('{:d} '.format(matrix[r][c]), end='')
+        else:
+            print()
